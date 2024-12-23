0a2a596fcd23c59ffe42e4322eba86045e6b26c7
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2024-12-23 11:45:35 +0100

    Remove old core and stdlib for a rewrite, leave UI and server as is.

--- src/delphyne/core/__init__.py
+++ src/delphyne/core/__init__.py
@@ -1,14 +1,3 @@
-from delphyne.core.queries import ParseError
-from delphyne.core.strategies import StrategyTree
-from delphyne.core.trees import Navigation, Node, Strategy, Success, Tree
-
-
-__all__ = [
-    "ParseError",
-    "StrategyTree",
-    "Strategy",
-    "Node",
-    "Tree",
-    "Navigation",
-    "Success",
-]
+"""
+Delphyne Core
+"""
