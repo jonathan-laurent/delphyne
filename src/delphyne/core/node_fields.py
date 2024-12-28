073530c2f3f486666f65753f0d1cb850e8de09ca
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2024-12-28 15:40:25 +0100

    Define node field types

--- src/delphyne/core/node_fields.py
+++ src/delphyne/core/node_fields.py
@@ -3,3 +3,52 @@ Internal utilities to detect node fields.
 """
 
 from dataclasses import dataclass
+
+
+####
+#### Fields
+####
+
+
+@dataclass
+class SpaceF:
+    """
+    A space field that does not correspond to am embedded nested tree.
+    """
+
+
+@dataclass
+class EmbeddedF:
+    """
+    A field corresponding to a nested embedded space.
+    """
+
+
+@dataclass
+class DataF:
+    """
+    A field that corresponds to some other kind of data.
+    """
+
+
+@dataclass
+class ParametricF:
+    """
+    A field corresponding to a parametric space.
+    """
+
+    res: SpaceF | EmbeddedF
+
+
+@dataclass
+class SequenceF:
+    """
+    A field corresponding to a sequence of spaces.
+    """
+
+    element: ParametricF | SpaceF | EmbeddedF
+
+
+####
+#### Inference Utilities
+####
---
+++ src/delphyne/core/opaque.py
@@ -0,0 +1,3 @@
+"""
+Opaque Spaces.
+"""
