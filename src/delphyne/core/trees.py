e770655d6b235bfbfdc205255a868505302d1a9e
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-01-02 14:16:13 +0100

    Move opaque spaces

--- src/delphyne/core/trees.py
+++ src/delphyne/core/trees.py
@@ -270,6 +270,31 @@ class EmbeddedTree[N: Node, P, T](Space[T]):
 
 
 #####
+##### Opaque Spaces
+#####
+
+
+@dataclass
+class OpaqueSpace[P, T](Space[T]):
+    stream: Callable[[PolicyEnv, P], StreamRet[T]]
+    _source: "StrategyComp[Any, P, T] | AttachedQuery[T]"
+
+    def source(self) -> "StrategyComp[Any, P, T] | AttachedQuery[T]":
+        return self._source
+
+    def tags(self) -> Sequence[Tag]:
+        return self._source.tags()
+
+    @staticmethod
+    def from_query():
+        assert False
+
+    @staticmethod
+    def from_strategy():
+        assert False
+
+
+#####
 ##### Builders and Spawners
 #####
 
