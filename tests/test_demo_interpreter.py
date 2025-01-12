9762bf07ac27c01f3eb41c9d2a6ee5cdecebbb70
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-01-12 13:37:28 +0100

    Add new selector test

--- tests/test_demo_interpreter.py
+++ tests/test_demo_interpreter.py
@@ -118,4 +118,4 @@ def test_query_demo(name: str, valid: bool):
 
 if __name__ == "__main__":
     # Entry point for the debugger (see "Debug Server Tests" configuration).
-    test_server("test_generate_pairs")
+    test_server("test_iterated")
