add74b5aadac0f5dc130b498906d60172b286404
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-01-16 18:54:27 +0100

    Progress on `find_invariant`

--- examples/find_invariants/find_invariants.py
+++ examples/find_invariants/find_invariants.py
@@ -2,6 +2,7 @@
 A simple Delphyne strategy to discover loop invariants with Why3.
 """
 
+from collections.abc import Sequence
 from dataclasses import dataclass
 
 import delphyne as dp
@@ -11,30 +12,67 @@ import why3_utils as why3
 
 
 @dataclass
-class FindInvariantsIP:
+class ProveProgramIP:
+    propose_change: "dp.Policy[Branch | Failure, ProposeChangeIP]"
+
+
+@dataclass
+class ProposeChangeIP:
     pass
 
 
 @dataclass
 class Proposal:
+    """
+    A proposal that was made to finish a proof.
+
+    The proposal contains a full annotated program (`annotated`) but
+    also a summary of the changes made, for easier comparison with
+    previous proposals when doing redundancy checks. The
+    `rejection_reason` field is not `None` when the proposal was
+    rejected before consideration and contains a reason for the
+    rejection, so that future attempts can try and avoid the same
+    mistake.
+    """
+
     change_summary: str
     annotated: why3.File
-    success: bool
+    rejection_reason: str | None
 
 
 @strategy
-def find_invariants(
+def prove_program(
     prog: why3.File,
-) -> Strategy[Branch | Factor | Failure, FindInvariantsIP, why3.File]:
-    annotated = prog
-    feedback = why3.check(prog, annotated)
-    yield from dp.ensure(feedback.error is None, "invalid program")
+) -> Strategy[Branch | Factor | Failure, ProveProgramIP, why3.File]:
+    annotated: why3.File = prog
     while True:
-        # We treat it like a proposal
-        proposal = yield from dp.branch(propose_change(prog, annotated))
-        pass
+        feedback = why3.check(prog, annotated)
+        yield from dp.ensure(feedback.error is None, "invalid program")
+        if feedback.success:
+            return annotated
+        # yield from dp.ensure()
+        proposal = yield from dp.branch(
+            dp.iterate(
+                lambda prior: propose_change(prog, annotated, feedback, prior)(
+                    ProveProgramIP, lambda p: p.propose_change)))  # fmt: skip
+        annotated = proposal.annotated
 
 
-# @strategy
-# def propose_change(prog: why3.File, annotated: why3.File, prior_attempts) -> Strategy[]:
-#     pass
+@strategy
+def propose_change(
+    prog: why3.File,
+    annotated: why3.File,
+    feedback: why3.Feedback,
+    prior_attempts: Sequence[Proposal] | None,
+) -> Strategy[
+    Branch | Failure,
+    ProposeChangeIP,
+    tuple[Proposal | None, Sequence[Proposal]],
+]:
+    assert False
+    yield
+
+
+@dataclass
+class EvaluateProposal(dp.Query[float]):
+    annotated: why3.File
