dae35c3a356897322f791c61bd4cbd2e7e70e16d
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-01-17 14:10:34 +0100

    Improve YAML list insertion logic and handle edge case when there is no empty line at the end of the file

--- vscode-ui/src/edit_utils.ts
+++ vscode-ui/src/edit_utils.ts
@@ -17,7 +17,11 @@ function indentString(str: string, level: number, indent: string): string {
     .join("\n");
 }
 
-// Insert a new element in a YAML list and return the position of the added element
+// Insert a new element in a YAML list and return the position of the added
+// element.
+//
+// TODO: this function was written via trial and error and it might benefit from
+// cleaning up the logic.
 export function insertYamlListElements(
   editor: vscode.TextEditor,
   listRange: vscode.Range,
@@ -43,16 +47,26 @@ export function insertYamlListElements(
   }
   let toInsert = elements.join("\n");
   toInsert = originalListEmpty ? "\n" + toInsert : toInsert + "\n";
+  let addedEmptyLine = false;
   editor.edit((editBuilder) => {
     if (originalListEmpty) {
       // We have to erase the current empty list that is in the document
       editBuilder.delete(listRange);
+    } else if (insertPos.line == editor.document.lineCount - 1) {
+      // There seems to be an edge case where the list is not empty but it is at
+      // the very end of the file and there is no empty line after it. In this
+      // case, we add an empty line at the end of the file before doing the
+      // change.
+      const lastLine = editor.document.lineAt(editor.document.lineCount - 1);
+      if (lastLine.text.trim() !== "") {
+        editBuilder.insert(lastLine.range.end, "\n");
+        addedEmptyLine = true;
+      }
     }
     editBuilder.insert(insertPos, toInsert);
   });
-  const firstInsertedLine = originalListEmpty
-    ? insertPos.line + 1
-    : insertPos.line;
+  const firstInsertedLine =
+    originalListEmpty || addedEmptyLine ? insertPos.line + 1 : insertPos.line;
   return new vscode.Position(firstInsertedLine, 2 * (parentIndentLevel + 2));
 }
 
