a912cda9d710083c4cd488205e4ff7bb6c904fcd
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-08-13 14:33:58 +0200

    Docstrings for chats

--- src/delphyne/core/chats.py
+++ src/delphyne/core/chats.py
@@ -1,3 +1,7 @@
+"""
+Types for representing LLM chat histories.
+"""
+
 from collections.abc import Sequence
 from dataclasses import dataclass
 from typing import Any, Literal
@@ -7,12 +11,20 @@ from delphyne.core.refs import Answer, Structured, ToolCall
 
 @dataclass(frozen=True)
 class OracleMessage:
+    """
+    Messge containing an oracle answer.
+    """
+
     kind: Literal["oracle"]
     answer: Answer
 
 
 @dataclass(frozen=True)
 class FeedbackMessage:
+    """
+    Message containing user feedback.
+    """
+
     kind: Literal["feedback"]
     category: str | None = None
     description: str | None = None
@@ -21,12 +33,24 @@ class FeedbackMessage:
 
 @dataclass(frozen=True)
 class ToolResult:
+    """
+    User message containing the result of a tool call previously
+    initiated by an LLM.
+    """
+
     kind: Literal["tool"]
     call: ToolCall
     result: str | Structured
 
 
 type AnswerPrefixElement = OracleMessage | FeedbackMessage | ToolResult
+"""
+LLM chat history element.
+"""
 
 
 type AnswerPrefix = Sequence[AnswerPrefixElement]
+"""
+An LLM chat history, to be passed to a query as an answer prefix (see
+`Query.query_prefix`).
+"""
