5b0f4f4d88d234ccfa469da8e5e48d07ce492ad2
jonathan-laurent <jonathan.laurent@cs.cmu.edu>
2025-08-15 11:33:10 +0200

    Add docstrings for opaque spaces

--- src/delphyne/stdlib/opaque.py
+++ src/delphyne/stdlib/opaque.py
@@ -1,6 +1,10 @@
+"""
+Definition of Opaque Spaces
+"""
+
 from collections.abc import Callable, Sequence
 from dataclasses import dataclass
-from typing import Any, Generic, TypeVar
+from typing import Any, Generic, TypeVar, override
 
 import delphyne.core as dp
 from delphyne.core.trees import NestedTreeSpawner, QuerySpawner
@@ -12,13 +16,32 @@ P = TypeVar("P", contravariant=True)
 
 @dataclass(frozen=True)
 class OpaqueSpace(Generic[P, T], dp.Space[T]):
+    """
+    A space defined by a mapping from the ambient inner policy to a
+    search stream.
+
+    Opaque spaces can be defined from strategy instances
+    (`StrategyInstance`) or from queries (`Query`) via the `using`
+    method. Crucially, policies are unaware of how an opaque space was
+    created, preserving abstraction.
+
+    Parameters:
+        P: Type parameter for the ambient inner policy type.
+        T: Type parameter for the element type.
+
+    Attributes:
+        stream: Maps the ambient inner policy to a search stream.
+    """
+
     stream: Callable[[dp.PolicyEnv, P], Stream[T]]
     _source: dp.NestedTree[Any, Any, T] | dp.AttachedQuery[T]
     _tags: Sequence[dp.Tag]
 
+    @override
     def source(self) -> dp.NestedTree[Any, Any, T] | dp.AttachedQuery[T]:
         return self._source
 
+    @override
     def tags(self) -> Sequence[dp.Tag]:
         return self._tags
 
@@ -27,6 +50,12 @@ class OpaqueSpace(Generic[P, T], dp.Space[T]):
         query: dp.AbstractQuery[T1],
         get_policy: Callable[[P1, Sequence[dp.Tag]], PromptingPolicy],
     ) -> "dp.SpaceBuilder[OpaqueSpace[P1, T1]]":
+        """
+        Create an opaque space from a query.
+
+        The `Query.using` method is a more ergonomic wrapper.
+        """
+
         def build(
             spawner: QuerySpawner, tags: Sequence[dp.Tag]
         ) -> OpaqueSpace[P1, T1]:
@@ -47,6 +76,12 @@ class OpaqueSpace(Generic[P, T], dp.Space[T]):
         strategy: dp.StrategyComp[N, P2, T1],
         get_policy: Callable[[P1, Sequence[dp.Tag]], Policy[N, P2]],
     ) -> "dp.SpaceBuilder[OpaqueSpace[P1, T1]]":
+        """
+        Create an opaque space from a strategy instance.
+
+        The `StrategyInstance.using` method is a more ergonomic wrapper.
+        """
+
         def build(
             spawner: NestedTreeSpawner, tags: Sequence[dp.Tag]
         ) -> OpaqueSpace[P1, T1]:
@@ -66,3 +101,6 @@ class OpaqueSpace(Generic[P, T], dp.Space[T]):
 
 
 type Opaque[P, T] = dp.SpaceBuilder[OpaqueSpace[P, T]]
+"""
+A convenience type alias for an opaque space builder.
+"""
