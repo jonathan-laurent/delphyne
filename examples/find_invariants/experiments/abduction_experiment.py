502308d9edff71f1a17c5ee3b7563de3c4444aa6
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2025-09-30 12:14:07 +0200

    Update abduction_experiment.py

--- examples/find_invariants/experiments/abduction_experiment.py
+++ examples/find_invariants/experiments/abduction_experiment.py
@@ -16,7 +16,7 @@ configs = [
     for seed in range(3)
     for num_completions in [8]
     for temp in [1.5]
-    for max_requests_per_attempt in [4, 8]
+    for max_requests_per_attempt in [4, 6, 8]
 ]
 
 if __name__ == "__main__":
